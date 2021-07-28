43e96f6480437637bcd0c5ba1a9c3d2e1631050b
Cleveland <eclevela@richmond.edu>
2021-07-28 17:45:15 -0400

    adding script for working wather data, cleaned up root directory a little

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
-function buildWdata(Day) {
-
-    // get data
-
-    d3.csv("cleanSales.csv").then(function (salesData) {
-        console.log(salesData);
-    });
 
 
+// d3.json("weatherdata.json").then(data => {
+//     console.log(data);
+//     var metadata = data.metadata;
+//     console.log(metadata);
+// })
 
-    d3.csv("RichmondWeather.csv").then(function (wData) {
-        console.log(wData);
-        var dates = wData.map(data => data.DATE)
 
-        console.log(dates);
+function buildWdata(sample) {
 
 
+
+
+    d3.json("weatherdata.json").then(wdata => {
+
+        var filteredData = wdata.filter(day => day.DATE == sample);
+    
+        console.log(filteredData);
+    
+var result = filteredData[0];
+
+console.log(result);
 
 
 
@@ -48,110 +54,110 @@ function buildWdata(Day) {
         //     var gaugeLayout = { width: 600, height: 400 };
         //     Plotly.newPlot('gauge', gaugeData, gaugeLayout);
 
-        //     var panel = d3.select("#weather-data");
+        var panel = d3.select("#weather-Data");
 
-        //     // ensure the panel is clear
-        //     panel.html("");
+        // ensure the panel is clear
+        panel.html("");
 
-        //     // use object.entries to access key value pairs
-        //     // use chain technique to add text to my h5 tag for each key value pair
-        //     Object.entries(result).forEach(([key, value]) => {
-        //       panel.append("h5").text(`${key.toUpperCase()}: ${value}`);
-        //     });
-
-        //     });
-        //   };
-
-        //     function buildCharts (sample) {
-        //       d3.json("samples.json").then(data => {
-        //         console.log(data);
-        //         // parse data out to variables
-        //         // * Use `sample_values` as the values for the bar chart.
-        //         // * Use `otu_ids` as the labels for the bar chart.
-        //         // * Use `otu_labels` as the hovertext for the chart.
-
-        //         var samples = data.samples;
-        //         console.log(samples);
-
-        //         var resultsArray = samples.filter(sampleObj => sampleObj.id == sample);
-        //         var result = resultsArray[0];
-
-        //         console.log(result);
-        //         var sample_values = result.sample_values;
-        //           console.log(sample_values)
-        //         var otu_ids = result.otu_ids;
-        //           console.log(otu_ids)
-        //         var otu_labels = result.otu_labels;
-        //           console.log(otu_labels)
-
-
-        //       // Build bar chart
-        //         // Use slice to get the top ten data
-
-        //         var yticks = otu_ids.slice(0,10).map(otuID => `OTU ${otuID}`).reverse();
-
-        //         var barData = [{
-        //           y: yticks,
-        //           x: sample_values.slice(0,10).reverse(),
-        //           text: otu_labels.slice(0,10).reverse(),
-        //           type: "bar",
-        //           orientation: "h",
-
-        //         }];
-
-        //         barLayout = {
-        //           title: "Top Ten Bacteria Cultures Found",
-        //           margin: {
-        //             t: 30,
-        //             r: 0,
-        //             b: 30,
-        //             l: 75
-        //           },
-
-        //         };
-
-
-
-
-        //         Plotly.newPlot("bar", barData, barLayout);
-
-        //       // Build bubble chart
-
-        //         var bubbleData = [{
-        //           x: otu_ids,
-        //           y: sample_values,
-
-        //           text: otu_labels,
-        //           mode: "markers",
-        //           marker: {
-        //             size:sample_values,
-        //             color: otu_ids,
-        //             colorscale: "Earth"
-        //           },
-
-        //           orientation: "h",
-        //         }];
-
-        //         var bubbleLayout = {
-        //             title:"Bacteria Cultures Per Sample",
-        //             margin: {
-        //               t: 30,
-        //               r: 10,
-        //               b: 30,
-        //               l: 75,
-        //             },
-        //             hovermode: "closest",
-        //             xaxis: {title: "OTU ID"},
-        //         };
-
-
-        //         Plotly.newPlot("bubble", bubbleData, bubbleLayout);
-
-
-
-
-
+        // use object.entries to access key value pairs
+        // use chain technique to add text to my h5 tag for each key value pair
+        Object.entries(result).forEach(([key, value]) => {
+          panel.append("h5").text(`${key}: ${value}`);
     });
+
+    //     });
+    //   };
+
+    //     function buildCharts (sample) {
+    //       d3.json("samples.json").then(data => {
+    //         console.log(data);
+    //         // parse data out to variables
+    //         // * Use `sample_values` as the values for the bar chart.
+    //         // * Use `otu_ids` as the labels for the bar chart.
+    //         // * Use `otu_labels` as the hovertext for the chart.
+
+    //         var samples = data.samples;
+    //         console.log(samples);
+
+    //         var resultsArray = samples.filter(sampleObj => sampleObj.id == sample);
+    //         var result = resultsArray[0];
+
+    //         console.log(result);
+    //         var sample_values = result.sample_values;
+    //           console.log(sample_values)
+    //         var otu_ids = result.otu_ids;
+    //           console.log(otu_ids)
+    //         var otu_labels = result.otu_labels;
+    //           console.log(otu_labels)
+
+
+    //       // Build bar chart
+    //         // Use slice to get the top ten data
+
+    //         var yticks = otu_ids.slice(0,10).map(otuID => `OTU ${otuID}`).reverse();
+
+    //         var barData = [{
+    //           y: yticks,
+    //           x: sample_values.slice(0,10).reverse(),
+    //           text: otu_labels.slice(0,10).reverse(),
+    //           type: "bar",
+    //           orientation: "h",
+
+    //         }];
+
+    //         barLayout = {
+    //           title: "Top Ten Bacteria Cultures Found",
+    //           margin: {
+    //             t: 30,
+    //             r: 0,
+    //             b: 30,
+    //             l: 75
+    //           },
+
+    //         };
+
+
+
+
+    //         Plotly.newPlot("bar", barData, barLayout);
+
+    //       // Build bubble chart
+
+    //         var bubbleData = [{
+    //           x: otu_ids,
+    //           y: sample_values,
+
+    //           text: otu_labels,
+    //           mode: "markers",
+    //           marker: {
+    //             size:sample_values,
+    //             color: otu_ids,
+    //             colorscale: "Earth"
+    //           },
+
+    //           orientation: "h",
+    //         }];
+
+    //         var bubbleLayout = {
+    //             title:"Bacteria Cultures Per Sample",
+    //             margin: {
+    //               t: 30,
+    //               r: 10,
+    //               b: 30,
+    //               l: 75,
+    //             },
+    //             hovermode: "closest",
+    //             xaxis: {title: "OTU ID"},
+    //         };
+
+
+    //         Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+
+
+
+
+
+});
 
 
 };
@@ -160,11 +166,11 @@ function buildWdata(Day) {
 
 function init() {
     var pullDownMenu = d3.select("#selDataset")
-    d3.csv("RichmondWeather.csv").then(function (wData) {
-        console.log(wData);
+    d3.json("weatherdata.json").then(function (wData) {
+        // console.log(wData);
         var days = wData.map(data => data.DATE)
 
-        console.log(days);
+
 
         days.forEach((day) => {
             pullDownMenu
@@ -177,13 +183,13 @@ function init() {
 
     });
 
-    buildWdata(940);
+    buildWdata("7/1/2017");
     // buildCharts(940);
 }
 
 function optionChanged(nextSample) {
-    buildMetadata(nextSample);
-    buildCharts(nextSample);
+    buildWdata(nextSample);
+    // buildCharts(nextSample);
 }
 init();
 
